1fcf457ae49df2237f6b9870a6fa97084be5d8a9
eminakcil <eminakcil@outlook.com>
2023-04-14 17:34:35 +0300

    workflow changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('this is amazing!!!');
+  res.send('i wanna print just text');
 });
 
 app.listen(PORT, HOST, () => {
