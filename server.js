f0a56a381d0247b7f449046281b647c904149b2e
eminakcil <eminakcil@outlook.com>
2023-04-24 10:47:32 +0300

    test 9999

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('i wanna print just text');
+  res.send('it\'s worked');
 });
 
 app.listen(PORT, HOST, () => {
